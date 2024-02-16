2ff0cf9bc3594e2e08b56fe70e3be45d3b8a247a
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2024-02-16 16:27:14 -0300

    fazer implementaçao de redefinir senha

--- WINecommerce/settings.py
+++ WINecommerce/settings.py
@@ -136,3 +136,5 @@ AUTH_USER_MODEL = 'venda.Usuario'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
