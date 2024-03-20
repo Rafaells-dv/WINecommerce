518b7d6ba6cff5ad96cd06c75bdd06b8a23c02c4
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2024-03-20 13:09:01 -0300

    Adjust credentials

--- venda/credentials/credentials.py
+++ venda/credentials/credentials.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 credentials_h = {
-            'client_id': 'Client_Id_3e8b0d2b528839d31913ae02f3982980e3fd1d74',
-            'client_secret': 'Client_Secret_304d68cc3dea0e1c2347c5594e0382e359e486fc',
+            'client_id': os.environ.get('CLIENT_ID_H'),
+            'client_secret': os.environ.get('CLIENT_SECRET_H'),
             'sandbox': True,
-            'certificate': 'D:\Efi\certificados\homologacao-539200-win - homo_cert.pem'
-        }
+            'certificate': os.environ.get('CERTIFICATE_H')
+}
 
 credentials_p = {
             'client_id': os.environ.get('CLIENT_ID_P'),
